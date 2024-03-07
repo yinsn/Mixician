Fix calculation in '_calculate_first_order_weights'

src/mixician/calculators/calculate_with_components.py
@@ -64,12 +64,14 @@ class LogarithmPCACalculator(BaseCalculator):
     def _data_preprocessing(self) -> None:
         """Preprocesses the input data for PCA calculation."""
         logger.info("Preprocessing data for PCA calculation...")
-        data = self.dataframe[self.score_columns].dropna().to_numpy()
+        self.data = self.dataframe[self.score_columns].dropna().to_numpy()
         if self.logarithm_transform:
             logger.info("Applying logarithm transformation to data...")
-            data = np.log10(self.logarithm_smoothing_term + data)
+            data = np.log10(self.logarithm_smoothing_term + self.data)
             self.logarithm_data_means = np.mean(data, axis=0)
-        data_centered = data - self.logarithm_data_means
+            data_centered = data - self.logarithm_data_means
+        else:
+            data_centered = self.data - np.mean(self.data, axis=0)
         if self.var_normalized:
             logger.info("Normalizing variables in data...")
             data_normalized = data_centered / np.std(data_centered, axis=0)

src/mixician/calculators/calculate_with_self_balancing_components.py
@@ -56,7 +56,8 @@ class SelfBalancingLogarithmPCACalculator(LogarithmPCACalculator):
     def _calculate_first_order_weights(self) -> None:
         """Calculates first-order weights for the PCA transformation."""
         self.first_order_weights = 10.0 ** (
-            self.target_distribution_mean - self.logarithm_data_means
+            self.target_distribution_mean / sum(self.power_weights)
+            - self.logarithm_data_means
         )
 
     def _calculate_power_weights(self) -> None:
@@ -67,13 +68,24 @@ class SelfBalancingLogarithmPCACalculator(LogarithmPCACalculator):
 
     def calculte_balanced_weights(self) -> None:
         """Calculates and applies balanced weights to the PCA components."""
-        self._calculate_first_order_weights()
         self._calculate_power_weights()
+        self._calculate_first_order_weights()
+
+    def _calculate_cumulative_product_scores(self) -> None:
+        """Calculates the cumulative product of the scores."""
+        logger.info("Calculating cumulative product of scores...")
+        self.cumulative_product_scores = 1 + np.multiply(
+            self.data, self.first_order_weights
+        )
+        self.cumulative_product_scores = self.cumulative_product_scores ** np.squeeze(
+            self.power_weights
+        )
+        self.cumulative_product_scores = np.prod(self.cumulative_product_scores, axis=1)
 
     def plot_self_balancing_projected_distribution(self) -> None:
         """Plots the projected data distribution after applying logarithm PCA and balancing weights."""
+        self._calculate_cumulative_product_scores()
         self.viewer_instance.plot_array_distribution(
-            scores=self.projected_data / self.projected_data_3sigma_std
-            + self.target_distribution_mean,
+            scores=np.log10(self.cumulative_product_scores),
             legend="Projected Data",
         )